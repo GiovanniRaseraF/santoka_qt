c0322a8082df4e72c441a55d7f5f1d26c7df9807
Giovanni <giovanni.raserafarra@gmail.com>
2023-01-26 00:37:41 -0800

    hpv1: testing with ui, conclusion, cannot proced like this

---
+++ apps/hpowerscreenV1/generalinfo_ui.cpp
@@ -0,0 +1,6 @@
+#include "generalinfo_ui.h"
+
+generalinfo_ui::generalinfo_ui(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ apps/hpowerscreenV1/generalinfo_ui.h
@@ -0,0 +1,17 @@
+#ifndef GENERALINFO_UI_H
+#define GENERALINFO_UI_H
+
+#include <QObject>
+
+class generalinfo_ui : public QObject
+{
+    Q_OBJECT
+public:
+    explicit generalinfo_ui(QObject *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // GENERALINFO_UI_H
