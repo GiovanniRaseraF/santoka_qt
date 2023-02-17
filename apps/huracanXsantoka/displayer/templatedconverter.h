4f7a1f9c9dd840453f772e1f6d1353bd550dffd0
GiovanniRaseraF <giovanni.raserafarra@gmail.com>
2023-02-17 09:37:30 +0100

    hXs: added support for mask on GCC 5

--- apps/huracanXsantoka/displayer/templatedconverter.h
+++ apps/huracanXsantoka/displayer/templatedconverter.h
@@ -4,6 +4,33 @@
 #include <utility>
 #include <QObject>
 
+#ifndef SANTOKA
+// NOT efficent, this runs a for every time
+// to emprove this i can use MEMOIZATION
+template<uint64_t s, uint64_t e>
+uint64_t mask(){
+    uint64_t ret = 0;
+
+    for(uint64_t i = s; i < e; i++) {
+       ret |= (0x8000000000000000 >> i);
+    }
+
+    return ret;
+}
+
+template<uint64_t sbyte, uint64_t ebyte>
+uint64_t maskbyte(){
+    uint64_t s = sbyte * 8, e = ebyte * 8;
+    uint64_t ret = 0;
+
+    for(uint64_t i = s; i < e; i++) {
+       ret |= (0x8000000000000000 >> i);
+    }
+
+    return ret;
+}
+#else
+// this is the better version because the filter is calculated at compile time
 template<uint64_t s, uint64_t e, uint64_t c>
 uint64_t mask(){
     if constexpr(c >= s && c < e)
@@ -26,5 +53,5 @@ template<uint64_t sbyte, uint64_t ebyte>
 uint64_t maskbyte(){
     return mask<sbyte * 8, ebyte * 8>();
 }
-
+#endif
 #endif // TEMPLATEDCONVERTER_H
