a9a112aae039b034e28e4469bc3a2a5c099b3965
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2024-05-24 11:39:33 +0200

    info: add compiler version for arm

--- apps/huracanXsantoka/pages/softwareinfo.cpp
+++ apps/huracanXsantoka/pages/softwareinfo.cpp
@@ -9,15 +9,19 @@ softwareinfo::softwareinfo(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    // info
+    ui->l_git_version->setText(GIT_VERSION);
+    ui->l_build_date->setText(BUILD_DATE);
+#ifdef SANTOKA
+    ui->l_compiler_version->setText("imx6guf-guf-linux-gnueabi");
+#else
+    ui->l_compiler_version->setText(COMPILER_VERSION);
+
     // debug
     qDebug() << "GIT_VERSION: " << GIT_VERSION;
     qDebug() << "BUILD_DATE: " << BUILD_DATE ;
     qDebug() << "COMPILER_VERSION: " << __VERSION__ << " - " << COMPILER_VERSION;
-
-    // info
-    ui->l_git_version->setText(GIT_VERSION);
-    ui->l_build_date->setText(BUILD_DATE);
-    ui->l_compiler_version->setText(COMPILER_VERSION);
+#endif
 }
 
 softwareinfo::~softwareinfo()
