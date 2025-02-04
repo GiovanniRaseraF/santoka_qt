65f886cb5bb7067944cb44473e6255f7480e74a0
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2025-02-04 16:00:01 +0100

    coverter: assing new tipe of converter

--- converter/send.sh
+++ converter/send.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+# with $1 you need to specify the file
+# with $2 the can number 0 or 1 you want to read from the file
+# examples:
+# ./send.sh file.log 1
+# read can1 from file and send it to vcan0
 
-canplayer -I $1 vcan0=can0
+canplayer -I $1 vcan0=can$2
--- converter/setup.sh
+++ converter/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+sudo ip link set down vcan0
 sudo modprobe vcan
-sudo ip link add dev vcan0 type vcan
+sudo ip link add dev vcan0 type vcan || 
 sudo ip link set up vcan0
 
-sudo ip link set can0 type can bitrate 250000
-sudo ip link set up can0
+# sudo ip link set down can0
+# sudo ip link set can0 type can bitrate 250000
+# sudo ip link set up can0
